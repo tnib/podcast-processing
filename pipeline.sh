74737cd1465490f067dcece84848ca60c1fced31
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2023-09-04 14:35:42 -0700

    Updated full pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -6,4 +6,6 @@ rm -rf ./transcript.txt
 python3 ./transcribe.py
 . ./generate_shownotes.sh
 . ./generate_title.sh
-. ./generate_twitter_link.sh
+. ./generate_twitter_post.sh
+. ./generate_facebook_post.sh
+. ./generate_linkedin_post.sh
